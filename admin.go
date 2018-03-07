314eb03872112a40fb10579bc163b8705df34281
aosfather <aosfather@163.com>
2018-03-07 23:50:04 +0800

    增加admin管理服务端口

---
+++ admin.go
@@ -0,0 +1,33 @@
+package bingo
+
+import (
+	"net/http"
+	"strconv"
+)
+
+type adminService struct {
+	application *TApplication
+}
+
+func (this *adminService) run(){
+	port, err := strconv.Atoi(this.application.context.getProperty("bingo.admin.port"))
+	if err != nil {
+		port=_DEFAULT_ADMIN_PORT
+	}
+	go http.ListenAndServe(":"+strconv.Itoa(port), this)
+}
+
+func (this *adminService)ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	writer.Write([]byte("admin"))
+}
+
+//执行shutdown操作
+func (this *adminService)doShutDown(writer http.ResponseWriter) {
+
+	//执行自定义
+	if this.application.onShutdown!=nil {
+		this.application.onShutdown()
+	}
+}
+
+type OnDestoryHandler func() bool //shutdown的handler，用于处理关闭服务的自定义动作
