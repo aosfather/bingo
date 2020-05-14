6bd33ccac5b4fb2ac81f7823fc0881da2ad8d086
aosfather <aosfather@gmail.com>
2020-05-14 23:14:06 +0800

    重新初始化项目

---
+++ run.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/aosfather/bingo_mvc/context"
+	"github.com/aosfather/bingo_mvc/fasthttp"
+)
+
+func main() {
+	boot := context.Boot{}
+	boot.Init(&fasthttp.FastHTTPDispatcher{}, load)
+	boot.Start()
+}
+func load() []interface{} {
+
+	return nil
+}
